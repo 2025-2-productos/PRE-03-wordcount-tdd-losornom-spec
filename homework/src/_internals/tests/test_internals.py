0a01cbd1681a878bb404c3e8da271d1e22ba0577
losornom-spec <losornom@unal.edu.co>
2025-11-29 12:40:05 -0500

    Continuacion

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -1,6 +1,14 @@
+import os
+import shutil
 import sys
 
-from ...wordcount import parse_args
+from ...wordcount import (
+    count_words,
+    parse_args,
+    preprocess_lines,
+    split_lines_into_words,
+    write_word_counts,
+)
 from ..read_all_files import read_all_files
 
 
@@ -25,3 +33,68 @@ def test_read_all_lines():
         "Analytics refers to the systematic computational analysis of data" in line
         for line in lines
     )
+
+
+def test_preprocess_lines():
+    lines = ["Hello, World!", "This is a TESTLINE."]
+    prepoccessed_lines = preprocess_lines(lines)
+    assert prepoccessed_lines == ["hello, world!", "this is a testline"]
+
+
+def test_split_into_words():
+    lines = ["hello world", "this is a testline"]
+    words = split_lines_into_words(lines)
+    assert words == [
+        "hello",
+        "world",
+        "this",
+        "is",
+        "a",
+        "testline",
+    ]
+
+
+def test_count_words():
+    words = [
+        "hello",
+        "world",
+        "this",
+        "is",
+        "a",
+        "testline",
+        "hello",
+        "world",
+    ]
+    word_count = count_words(words)
+    assert word_count == {
+        "hello": 2,
+        "world": 2,
+        "this": 1,
+        "is": 1,
+        "a": 1,
+        "testline": 1,
+    }
+
+
+def test_write_word_counts():
+    output_folder = "data/output/"
+    word_count = {"hello": 2, "world": 2, "this": 1}
+
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+
+    write_word_counts(output_folder, word_count)
+
+    output_file = os.path.join(output_folder, "wordcount.tsv")
+
+    assert os.path.exists(output_file), "Output file was not created."
+
+    with open(output_file, "r") as f:
+        lines = f.readlines()
+    assert lines == ["hello\t2\n", "world\t2\n", "this\t1\n"]
+
+    assert os.path.exists(output_file), "Output file was not created."
+
+    with open(output_file, "r") as f:
+        lines = f.readlines()
+    assert lines == ["hello\t2\n", "world\t2\n", "this\t1\n"]
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,9 +1,7 @@
 # Ejemplo para casos de uso
 # python3 -m homework data/input data/output
 
-
 import argparse
-import sys
 
 from homework.src._internals.read_all_files import read_all_files
 
@@ -24,6 +22,27 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
+def preprocess_lines(lines):
+    preprocessed = [line.lower() for line in lines]
+    return preprocessed
+
+
+def split_lines_into_words(lines):
+    pass
+
+
+def count_words(words):
+    pass
+
+
+def write_word_counts(output_folder, word_count):
+    pass
+
+
 def main():
     input_folder, output_folder = parse_args()
     lines = read_all_files(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_lines_into_words(preprocessed_lines)
+    word_count = count_words(words)
+    write_word_counts(output_folder, word_count)
